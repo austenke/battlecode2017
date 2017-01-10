27aebbde0af4dbfd13b06c77eb2a09b4312d5198
Austen Keene <austenkeene@gmail.com>
2017-01-10 16:53:56 -0500

    Initial push

---
+++ src/examplefuncsplayer/RobotPlayer.java
@@ -0,0 +1,40 @@
+package examplefuncsplayer;
+import battlecode.common.*;
+import Helpers.HelperMethods;
+import Robots.*;
+import com.sun.tools.corba.se.idl.toJavaPortable.Helper;
+
+public strictfp class RobotPlayer {
+    static RobotController rc;
+    static HelperMethods helpers;
+
+    /**
+     * run() is the method that is called when a robot is instantiated in the Battlecode world.
+     * If this method returns, the robot dies!
+    **/
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+
+        // This is the RobotController object. You use it to perform actions from this robot,
+        // and to get information on its current status.
+        RobotPlayer.rc = rc;
+        helpers = new HelperMethods(rc);
+
+        // Here, we've separated the controls into a different method for each RobotType.
+        // You can add the missing ones or rewrite this into your own control structure.
+        switch (rc.getType()) {
+            case ARCHON:
+                new Archon(rc, helpers).run();
+                break;
+            case GARDENER:
+                new Gardener(rc, helpers).run();
+                break;
+            case SOLDIER:
+                new Soldier(rc, helpers).run();
+                break;
+            case LUMBERJACK:
+                new Lumberjack(rc, helpers).run();
+                break;
+        }
+	}
+}
