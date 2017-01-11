b248af3acd30083934035b0f2dc7c542cf370e39
dan-jfisher <dan.johnfisher@gmail.com>
2017-01-10 21:42:46 -0500

    archon update

--- src/Robots/Archon.java
+++ src/Robots/Archon.java
@@ -3,6 +3,8 @@ package Robots;
 import Helpers.HelperMethods;
 import battlecode.common.*;
 
+import static Helpers.HelperMethods.randomDirection;
+
 /**
  * Class for archon robot.
  */
@@ -25,20 +27,23 @@ public class Archon {
             try {
 
                 // Generate a random direction
-                Direction dir = helpers.randomDirection();
-
-                // Randomly attempt to build a gardener in this direction
-                if (rc.canHireGardener(dir) && rc.getRobotCount() < 6) {
+                Direction dir = randomDirection();
+                if (rc.getRoundNum() <= 100) {
+                    if (rc.canHireGardener(dir) && rc.getTeamBullets() >= 200) {
+                        rc.hireGardener(dir);
+                    }
+                } else if (rc.getTeamBullets() >= 800) {
+                    rc.donate(400);
+                } else if (rc.canHireGardener(dir) && rc.getTeamBullets() >= 300 && (rc.getTeamVictoryPoints() / rc.getRobotCount()) > 30) {
                     rc.hireGardener(dir);
                 }
 
-                // Move randomly
-                helpers.tryMove(helpers.randomDirection());
+                //tryMove(randomDirection());
 
                 // Broadcast archon's location for other robots on the team to know
-                MapLocation myLocation = rc.getLocation();
+                /*MapLocation myLocation = rc.getLocation();
                 rc.broadcast(0,(int)myLocation.x);
-                rc.broadcast(1,(int)myLocation.y);
+                rc.broadcast(1,(int)myLocation.y);*/
 
                 // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
                 Clock.yield();
