1a85b7c54d6bffa12d934a0a4c7e93035ba6de57
dan-jfisher <dan.johnfisher@gmail.com>
2017-01-12 10:51:37 -0500

    functioning tank

--- src/Robots/Tank.java
+++ src/Robots/Tank.java
@@ -14,26 +14,36 @@ public class Tank {
 
     public static void run() throws GameActionException{
 
-        while(true){
-//            MapLocation[] blocs = rc.senseBroadcastingRobotLocations();  //broadcasting locations
-//            RobotInfo[] nlocs = rc.senseNearbyRobots(10, rc.getTeam().opponent());  //nearby locations
-//            MapLocation floc = blocs[0];
-//            MapLocation Archon = rc.getInitialArchonLocations(rc.getTeam())[0];
-//
-//            if(nlocs.length == 0) {
-//                System.out.println("Tank code");
-//                //major flaw: if no enemy robots are broadcasting, tank will move towards its furthest ally
-//                for(MapLocation loc: blocs){
-//                    if(Archon.distanceTo(loc) > Archon.distanceTo(floc)){
-//                        floc = loc;
-//                    }
-//                }
-//                Direction enemyDir = rc.getLocation().directionTo(floc);
-//                helpers.tryMove(enemyDir);
-//            }else if (rc.canFireTriadShot()) {
-//                rc.fireTriadShot(rc.getLocation().directionTo(nlocs[0].getLocation()));
-//            }
+        try {
+            while (true) {
+                MapLocation[] blocs = rc.senseBroadcastingRobotLocations();  //broadcasting locations
+                RobotInfo[] nlocs = rc.senseNearbyRobots(10, rc.getTeam().opponent());  //nearby locations
+                MapLocation floc = rc.getLocation();
+                MapLocation Archon = rc.getInitialArchonLocations(rc.getTeam())[0];
+                MapLocation enemyArchon = rc.getInitialArchonLocations(rc.getTeam().opponent())[0];
 
+                if (nlocs.length == 0) {
+                    System.out.println("Tank code");
+                    //major flaw: if no enemy robots are broadcasting, tank will move towards its furthest ally
+                    if(blocs.length != 0){
+                        for (MapLocation loc : blocs) {
+                            if (Archon.distanceTo(loc) > Archon.distanceTo(floc)) {
+                                floc = loc;
+                            }
+                        }
+                        Direction enemyDir = rc.getLocation().directionTo(floc);
+                        helpers.tryMove(enemyDir);
+                    } else{
+                        helpers.tryMove(rc.getLocation().directionTo(enemyArchon));
+                    }
+                } else if (rc.canFireTriadShot()) {
+                    rc.fireTriadShot(rc.getLocation().directionTo(nlocs[nlocs.length-1].getLocation()));
+                }
+                Clock.yield();
+            }
+        }catch(Exception e) {
+            System.out.println("Soldier Exception");
+            e.printStackTrace();
         }
     }
 }
