79911a35d75089ab6ae18f753f8c33d3b9681f11
Austen Keene <austenkeene@gmail.com>
2017-01-13 18:19:58 -0500

    Add scout code

--- src/Robots/Scout.java
+++ src/Robots/Scout.java
@@ -1,8 +1,7 @@
 package Robots;
 
 import Helpers.HelperMethods;
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import battlecode.common.*;
 
 /**
  * Class for scout robot
@@ -10,6 +9,7 @@ import battlecode.common.RobotController;
 public class Scout {
     static RobotController rc;
     static HelperMethods helpers;
+    static Direction goingDirection;
 
     public Scout(RobotController rc, HelperMethods helpers) {
         this.rc = rc;
@@ -17,8 +17,19 @@ public class Scout {
     }
 
     public static void run() throws GameActionException {
+        goingDirection = HelperMethods.randomDirection();
         // The code you want your robot to perform every round should be in this loop
         while (true) {
+            goingDirection = roam(goingDirection);
         }
+    }
+
+    public static Direction roam(Direction dir) throws GameActionException {
+        return HelperMethods.stayInLocationRange(dir, rc.getLocation(), 30, 1000);
+    }
+
+    public static void detect() {
+        RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+
     }
 }
