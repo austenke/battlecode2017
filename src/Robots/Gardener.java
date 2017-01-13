9fedd4f31339c95530148349ae838dfb2b9206b6
Austen Keene <austenkeene@gmail.com>
2017-01-13 01:44:20 -0500

    Abstracted different sections of code and general cleanup

--- src/Robots/Gardener.java
+++ src/Robots/Gardener.java
@@ -4,9 +4,6 @@ import Helpers.HelperMethods;
 import Main.RobotPlayer;
 import battlecode.common.*;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 /**
  * Class for gardener robot.
  */
@@ -28,11 +25,11 @@ public class Gardener {
         int numSoldiers = 0;
         if(gardenerCount > 2 && ((gardenerCount + 1) % 2) == 0) {
             buildOrPlant = 0;
-            System.out.println("I'm a builder gardener!");
+            //System.out.println("I'm a builder gardener!");
         }
         else {
             buildOrPlant = 1;
-            System.out.println("I'm a planter gardener!");
+            //System.out.println("I'm a planter gardener!");
         }
         rc.broadcast(2,gardenerCount + 1);
 
@@ -51,27 +48,22 @@ public class Gardener {
 
             // Try/catch blocks stop unhandled exceptions, which cause your robot to explode
             try {
-                if (!rc.getLocation().isWithinDistance(myArchon, 30)) {
-                    float toRotate = (float) Math.random() * 70;
-                    Direction archonDir = rc.getLocation().directionTo(myArchon);
-                    if (Math.random() > .5) {
-                        archonDir = archonDir.rotateLeftDegrees(toRotate);
+                int minDist = -1;
+                int maxDist = -1;
+                if (!tryToWater()) {
+                    if (buildOrPlant == 0) {
+                        numSoldiers = builderGardener(numSoldiers);
+                        minDist = 20;
+                        maxDist = 30;
+                    } else if (buildOrPlant == 1) {
+                        planterGardener();
+                        minDist = 1;
+                        maxDist = 19;
                     }
-                    else {
-                        archonDir = archonDir.rotateRightDegrees(toRotate);
-                    }
-                    goingDir = archonDir;
 
-                    if(!rc.canMove(goingDir)){
-                        goingDir = HelperMethods.randomDirection();
+                    if (!rc.hasMoved()) {
+                        goingDir = HelperMethods.stayInArchonRange(goingDir, myArchon, minDist, maxDist);
                     }
-                    HelperMethods.tryMove(goingDir);
-                }
-                else if (buildOrPlant == 0) {
-                    numSoldiers = builderGardener(numSoldiers);
-                }
-                else if (buildOrPlant == 1) {
-                    planterGardener();
                 }
 
                 // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
@@ -79,6 +71,7 @@ public class Gardener {
 
             } catch (Exception e) {
                 System.out.println("Gardener Exception");
+                System.out.println("Has moved error: " + rc.hasMoved());
                 e.printStackTrace();
             }
         }
@@ -86,79 +79,39 @@ public class Gardener {
 
     static int builderGardener(int soldiers) throws GameActionException {
         int soldierCount = soldiers;
-        boolean watering = false;
 
-        TreeInfo[] trees = rc.senseNearbyTrees(-1, rc.getTeam());
-        TreeInfo treeToWater = null;
-
-        if (trees.length > 0) {
-            for (TreeInfo tree : trees) {
-                if (tree.getHealth() < GameConstants.BULLET_TREE_MAX_HEALTH - (GameConstants.WATER_HEALTH_REGEN_RATE * 2)) {
-                    if (treeToWater == null || treeToWater.getHealth() > tree.getHealth()) {
-                        treeToWater = tree;
-                    }
+        if (!rc.getLocation().isWithinDistance(myArchon, 20)) {
+            // Generate a random direction
+            Direction dir = helpers.randomDirection();
+            // Randomly attempt to build a soldier or lumberjack in this direction
+            if (rc.getRobotCount() < 15 || rc.getTeamBullets() >= 300) {
+                if (rc.canBuildRobot(RobotType.SOLDIER, dir) && soldierCount % 5 > 0) {
+                    rc.buildRobot(RobotType.SOLDIER, dir);
+                } else if (rc.canBuildRobot(RobotType.TANK, dir)) {
+                    rc.buildRobot(RobotType.TANK, dir);
                 }
-            }
-
-            if (treeToWater != null) {
-                watering = true;
-                if (rc.canWater(treeToWater.getLocation())) {
-                    int turnsToWater = (int) Math.floor((GameConstants.BULLET_TREE_MAX_HEALTH - treeToWater.getHealth())
-                            / GameConstants.WATER_HEALTH_REGEN_RATE);
-                    for (int i = 0; i < turnsToWater; i++) {
-                        rc.water(treeToWater.getLocation());
-                        Clock.yield();
-                    }
-                } else {
-                    helpers.tryMove(rc.getLocation().directionTo(treeToWater.getLocation()));
-                    Clock.yield();
-                }
-            }
-        }
-
-        if (!watering) {
-            if (!rc.getLocation().isWithinDistance(myArchon, 20)) {
-                // Generate a random direction
-                Direction dir = helpers.randomDirection();
-
-                // Randomly attempt to build a soldier or lumberjack in this direction
-                if (rc.getRobotCount() < 15 || rc.getTeamBullets() >= 300) {
-                    if (rc.canBuildRobot(RobotType.SOLDIER, dir) && soldierCount % 5 > 0) {
-                        rc.buildRobot(RobotType.SOLDIER, dir);
-                    } else if (rc.canBuildRobot(RobotType.TANK, dir)) {
-                        rc.buildRobot(RobotType.TANK, dir);
-                    }
-                    soldierCount++;
-                }
-                helpers.tryMove(helpers.randomDirection());
-            }
-            else {
-                float toRotate = (float) Math.random() * 70;
-                Direction oppArchonDir = rc.getLocation().directionTo(myArchon).opposite();
-                if (Math.random() > .5) {
-                    oppArchonDir = oppArchonDir.rotateLeftDegrees(toRotate);
-                }
-                else {
-                    oppArchonDir = oppArchonDir.rotateRightDegrees(toRotate);
-                }
-                goingDir = oppArchonDir;
-
-                if(!rc.canMove(goingDir)){
-                    goingDir = HelperMethods.randomDirection();
-                }
-                HelperMethods.tryMove(goingDir);
+                soldierCount++;
             }
         }
         return soldierCount;
     }
 
     static void planterGardener() throws GameActionException {
-        boolean watering = false;
 
         TreeInfo[] trees = rc.senseNearbyTrees(-1, rc.getTeam());
-        TreeInfo treeToWater = null;
 
+        if (rc.getTreeCount() < 20) {
+            tryToPlant();
+        }
+    }
+
+    public static boolean tryToWater() throws GameActionException {
+        TreeInfo[] trees = rc.senseNearbyTrees(-1, rc.getTeam());
+
+        // trees in area, need to filter for low hp trees
         if (trees.length > 0) {
+            // set to null, if there are no low hp trees in area then it will stay null
+            TreeInfo treeToWater = null;
             for (TreeInfo tree : trees) {
                 if (tree.getHealth() < GameConstants.BULLET_TREE_MAX_HEALTH - (GameConstants.WATER_HEALTH_REGEN_RATE * 2)) {
                     if (treeToWater == null || treeToWater.getHealth() > tree.getHealth()) {
@@ -168,64 +121,32 @@ public class Gardener {
             }
 
             if (treeToWater != null) {
-                watering = true;
                 if (rc.canWater(treeToWater.getLocation())) {
-                    int turnsToWater = (int) Math.floor((GameConstants.BULLET_TREE_MAX_HEALTH - treeToWater.getHealth())
+                    int turnsToWater = (int) Math.ceil((GameConstants.BULLET_TREE_MAX_HEALTH - treeToWater.getHealth())
                             / GameConstants.WATER_HEALTH_REGEN_RATE);
                     for (int i = 0; i < turnsToWater; i++) {
-                        rc.water(treeToWater.getLocation());
-                        Clock.yield();
+                        if (rc.canWater(treeToWater.getLocation())) {
+                            rc.water(treeToWater.getLocation());
+                            Clock.yield();
+                        }
+                        else {
+                            helpers.tryMove(rc.getLocation().directionTo(treeToWater.getLocation()));
+                            Clock.yield();
+                        }
                     }
                 } else {
                     helpers.tryMove(rc.getLocation().directionTo(treeToWater.getLocation()));
-                    Clock.yield();
                 }
+                // Return true for watering
+                return true;
             }
         }
-
-        if (!watering) {
-            if (!rc.getLocation().isWithinDistance(myArchon, 20)) {
-                float toRotate = (float) Math.random() * 70;
-                Direction archonDir = rc.getLocation().directionTo(myArchon);
-                if (Math.random() > .5) {
-                    archonDir = archonDir.rotateLeftDegrees(toRotate);
-                }
-                else {
-                    archonDir = archonDir.rotateRightDegrees(toRotate);
-                }
-                goingDir = archonDir;
-
-                if(!rc.canMove(goingDir)){
-                    goingDir = HelperMethods.randomDirection();
-                }
-                HelperMethods.tryMove(goingDir);
-            }
-            else {
-                if (rc.getTreeCount() < 15) {
-                    tryToPlant();
-                    if (!rc.canMove(goingDir)) {
-                        goingDir = HelperMethods.randomDirection();
-                    }
-                    HelperMethods.tryMove(goingDir);
-                } else {
-                    if (trees.length > 0) {
-                        if (noTreeInRange(4)) {
-                            int random = (int) Math.floor(Math.random() * (trees.length - 1));
-                            helpers.tryMove(rc.getLocation().directionTo(trees[random].getLocation()));
-                        } else {
-                            helpers.tryMove(HelperMethods.randomDirection());
-                        }
-                    } else {
-                        helpers.tryMove(HelperMethods.randomDirection());
-                    }
-                }
-            }
-        }
+        return false;
     }
 
-    public static void tryToPlant() throws GameActionException{
+    public static void tryToPlant() throws GameActionException {
         Direction[] dirList = RobotPlayer.getDirList();
-        if(rc.getTeamBullets()>GameConstants.BULLET_TREE_COST && rc.getLocation().distanceTo(myArchon) > 3) {//have enough bullets. assuming we haven't built already.
+        if(rc.getTeamBullets()>GameConstants.BULLET_TREE_COST && rc.getLocation().distanceTo(myArchon) > 2) {//have enough bullets. assuming we haven't built already.
             for (int i = 0; i < 4; i++) {
                 //only plant trees on a sub-grid
                 MapLocation p = rc.getLocation().add(dirList[i],GameConstants.GENERAL_SPAWN_OFFSET+GameConstants.BULLET_TREE_RADIUS+rc.getType().bodyRadius);
