2b840fdffba6b8b95fcbc66616827c3e4cd29af0
dan-jfisher <dan.johnfisher@gmail.com>
2017-01-25 08:40:33 -0500

    tank aggression

--- src/Robots/Tank.java
+++ src/Robots/Tank.java
@@ -22,39 +22,20 @@ public class Tank {
                 try {
                     rc.broadcast(0,(int)rc.getLocation().x);
                     rc.broadcast(1,(int)rc.getLocation().y);
-                    MapLocation[] blocs = rc.senseBroadcastingRobotLocations();  //broadcasting locations
-                    RobotInfo[] nlocs = rc.senseNearbyRobots(10, rc.getTeam().opponent());  //nearby locations
-                    MapLocation floc = rc.getLocation();
+                    RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(10, rc.getTeam().opponent());  //nearby locations
                     MapLocation Archon = rc.getInitialArchonLocations(rc.getTeam())[0];
                     MapLocation enemyArchon = rc.getInitialArchonLocations(rc.getTeam().opponent())[0];
 
-                    if (nlocs.length == 0) {
-                        //System.out.println("Tank code");
-                        //major flaw: if no enemy robots are broadcasting, tank will move towards its furthest ally
-                        if(blocs.length != 0){
-                            for (MapLocation loc : blocs) {
-                                if (Archon.distanceTo(loc) > Archon.distanceTo(floc)) {
-                                    floc = loc;
-                                }
-                            }
-                            Direction enemyDir = rc.getLocation().directionTo(floc);
-                            if(enemyDir != null){
-                                move.moveToLoc(floc);
-                            }
-                            else{
-                                move.tankMove();
-                            }
-                        }else{
-                            move.tankMoveToLoc(enemyArchon);
-                        }
+                    if (nearbyEnemies.length == 0) {
+                        move.tankMoveToLoc(enemyArchon);
                     } else {
                         if (rc.canFireTriadShot()) {
-                            rc.fireTriadShot(rc.getLocation().directionTo(nlocs[nlocs.length - 1].getLocation()));
+                            rc.fireTriadShot(rc.getLocation().directionTo(nearbyEnemies[nearbyEnemies.length - 1].getLocation()));
                         }
                         if (rc.canFireSingleShot()){
-                            rc.fireSingleShot(rc.getLocation().directionTo(nlocs[nlocs.length - 1].getLocation()));
+                            rc.fireSingleShot(rc.getLocation().directionTo(nearbyEnemies[nearbyEnemies.length - 1].getLocation()));
                         }
-                        move.tankStayInLocationRange(nlocs[0].getLocation(),
+                        move.tankStayInLocationRange(nearbyEnemies[0].getLocation(),
                                 (int) rc.getType().sensorRadius - 1, (int) rc.getType().sensorRadius);
                     }
                     Clock.yield();
